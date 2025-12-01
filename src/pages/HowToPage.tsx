6c811cb09d094a654991686bc663c0a356a8f342
Sri Ranga Bharadwaj Chakilam <chakilamsriranga@gmail.com>
2025-12-01 17:02:42 -0500

    feat: Add routing functionality to HowToPage by importing Link component

--- src/pages/HowToPage.tsx
+++ src/pages/HowToPage.tsx
@@ -1,5 +1,6 @@
 
 import React, { useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useContent } from '@/hooks/useContent';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
